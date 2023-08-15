1fd86e9e218d7f43184eec65175a12ddc15a15e2
sergyperov <sergyperov@gmail.com>
2023-08-15 14:55:16 +0400

    type: added downloadSignal to typings

--- types.d.ts
+++ types.d.ts
@@ -4,7 +4,18 @@ interface IGenArtPlatformCallbacks {
     onCapturePreview?: () => void
 }
 
+interface ISignal {
+    type: string
+}
+
+interface IDownloadSignal extends ISignal {
+    type: "gps:f:download"
+    key: string
+    text: string
+}
+
 export declare class GenArtPlatform {
     constructor(iframe: HTMLIFrameElement, callbacks: IGenArtPlatformCallbacks);
     triggerDownload: (key: string) => void;
+    downloadSignals: IDownloadSignal[]
 }
